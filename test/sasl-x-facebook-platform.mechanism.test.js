b89865fd29b67ef14383ea47d3ffbdf5395d57d9
Jared Hanson <jaredhanson@gmail.com>
2012-12-23 13:09:32 -0800

    Initial commit.

---
+++ test/sasl-x-facebook-platform.mechanism.test.js
@@ -0,0 +1,32 @@
+(function(root, factory) {
+  if (typeof exports === 'object') {
+    // CommonJS
+    factory(require('sasl-x-facebook-platform/lib/mechanism'));
+  } else if (typeof define === 'function' && define.amd) {
+    // AMD
+    define(['sasl-x-facebook-platform/lib/mechanism'], factory);
+  }
+}(this, function(Mechanism) {
+
+  describe('Mechanism', function() {
+    var mech = new Mechanism();
+    
+    it('should be named X-FACEBOOK-PLATFORM', function() {
+      expect(mech.name).to.equal('X-FACEBOOK-PLATFORM');
+    });
+  });
+  
+  describe('response to challenge', function() {
+    var mech = new Mechanism();
+    mech.challenge('method=GET&nonce=ABCDEFG');
+    
+    it('should encode credentials', function() {
+      var enc = mech.response({ apiKey: 'app123', token: 'secret' });
+      console.log('enc: ' + enc);
+      expect(enc).to.equal('method=GET&nonce=ABCDEFG&api_key=app123&access_token=secret&call_id=0&v=1.0');
+    });
+  });
+  
+  return { name: 'test.sasl-x-facebook-platform.mechanism' };
+  
+}));
---
+++ test/sasl-x-facebook-platform.test.js
@@ -0,0 +1,25 @@
+(function(root, factory) {
+  if (typeof exports === 'object') {
+    // CommonJS
+    factory(require('sasl-x-facebook-platform'));
+  } else if (typeof define === 'function' && define.amd) {
+    // AMD
+    define(['sasl-x-facebook-platform'], factory);
+  }
+}(this, function(saslxfacebookplatform) {
+
+  describe('sasl-x-facebook-platform', function() {
+    
+    it('should export Mechanism', function() {
+      expect(saslxfacebookplatform.Mechanism).to.be.a('function');
+    });
+    
+    it('should export Mechanism via module', function() {
+      expect(saslxfacebookplatform).to.equal(saslxfacebookplatform.Mechanism);
+    });
+    
+  });
+  
+  return { name: 'test.sasl-x-facebook-platform' };
+  
+}));
